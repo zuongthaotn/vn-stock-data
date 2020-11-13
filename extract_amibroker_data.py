3b728c0262ad5c82372a650940205e1b8505e4a8
zuongthao <zuongthao@gmail.com>
2020-11-13 08:36:33 +0700

    update data 12.11.2020

--- extract_amibroker_data.py
+++ extract_amibroker_data.py
@@ -4,15 +4,25 @@ import os
 import numpy as np
 import datetime
 
-dateparse = lambda x: datetime.datetime.strptime(x, '%Y%m%d')
 
 path = os.getcwd()
+amibroker_file = path + '/amibroker_all_data.txt'
+
+with open(amibroker_file) as f:
+    lines = f.readlines()
+
+lines[0] = "<Ticker>,<DTYYYYMMDD>,<Open>,<High>,<Low>,<Close>,<Volume>\n"
+
+with open(amibroker_file, "w") as f:
+    f.writelines(lines)
+
 if platform.system() == 'Windows':
     file = path + '\\VNX.csv'
 if platform.system() != 'Windows':
     file = path + '/VNX.csv'
-    #
-amibroker_file = path + '/amibroker_all_data.txt'
+#
+#
+dateparse = lambda x: datetime.datetime.strptime(x, '%Y%m%d')
 amibroker_data = pd.read_csv(amibroker_file, parse_dates=['<DTYYYYMMDD>'], date_parser=dateparse)
 amibroker_data.columns = ['Ticker', 'Date', 'Open', 'High', 'Low', 'Close', 'Volume']
 vnx_csv_data = pd.read_csv(file, usecols=["ticker"])
