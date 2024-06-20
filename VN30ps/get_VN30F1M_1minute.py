596eecaed0d79b67766a99ac98d2d9676fd2c0c0
zuongthao <thao.duong@snaptec.io>
2024-06-20 17:23:28 +0700

    analytics

--- VN30ps/get_VN30F1M_1minute.py
+++ VN30ps/get_VN30F1M_1minute.py
@@ -3,12 +3,12 @@ import warnings
 warnings.filterwarnings('ignore')
 
 import vn_realtime_stock_data.stockHistory as stockHistory
-import AI.constants as constants
+from pathlib import Path
 from datetime import datetime
 
 ticker = "VN30F" + str(datetime.now().strftime('%y')) + str(datetime.now().strftime('%m'))
+STOCK_DATA_DIR = Path(__file__).parent
 
 prepared_data = stockHistory.getVN30HistoryDataByMinute(ticker=ticker, resolution=1)
-vn_stock_data_folder = str(constants.ALGO_DIR) + '/vn-stock-data'
-prepared_data.to_csv(str(vn_stock_data_folder) + '/VN30F1M_1minute.csv')
+prepared_data.to_csv(str(STOCK_DATA_DIR) + '/VN30F1M_1minute.csv')
 exit()
